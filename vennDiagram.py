fb39cd0156c2ea899358a8b667627de5f1f718fb
Dylan <dylan.odriscoll@gmail.com>
2016-09-03 14:49:49 +0100

    Will write contents of playlist to file and print to terminal

--- vennDiagram.py
+++ vennDiagram.py
@@ -1,6 +1,13 @@
 import spotipy
 import spotipy.util as util
 
+def write_tracks(tracks, f):
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        f.write(("   %d %32.32s %s\n" % (i, track['artists'][0]['name'], track['name'])).encode('utf8'))
+        print("   %d %32.32s %s" % (i, track['artists'][0]['name'], track['name']))
+    f.close()
+
 scope = 'user-library-read'
 username = '1159991532'
 
@@ -8,6 +15,11 @@ token = util.prompt_for_user_token(username, scope)
 
 
 spotify = spotipy.Spotify(auth=token)
-username = '1159991532'
+username1 = '1159991532'
+username2 = 'petrichortardis'
 playlist1 = '14VcSdE3lgDQYuXA6GzpHU'
-results = spotify.user_playlist(username, playlist1, fields = "tracks")
+playlist2 = '1cHxarRhQFiANkZE3ZZiy5'
+results = spotify.user_playlist(username2, playlist2, fields = "tracks, next")
+tracks = results['tracks']
+with open('out.txt', 'wb') as f:
+    write_tracks(tracks, f)
